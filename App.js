f89ab17794214fff6549a052620d581ba3b619b7
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2023-07-14 09:48:38 -0600

    create network connection monitoring

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import Start from './components/Start';
 import Chat from './components/Chat';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, enableNetwork, disableNetwork } from 'firebase/firestore';
 
+//Import react, react-native, netinfo components
+import { useNetInfo } from '@react-native-community/netinfo';
+import { useEffect } from 'react';
+import { Alert } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+
+  // Check network status
+  const connectionStatus = useNetInfo();
+
+  useEffect(() => {
+    if (connectionStatus.isConnected === false) {
+      Alert.alert('Connection lost!');
+      disableNetwork(db);
+    } else if (connectionStatus.isConnected === true) {
+      enableNetwork(db);
+    }
+  }, [connectionStatus.isConnected]);
+
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
@@ -38,7 +54,7 @@ const App = () => {
           <Stack.Screen
             name='Chat'
           >
-            {props => <Chat db={db} {...props}/>}
+            {props => <Chat isConnected={connectionStatus.isConnected} db={db} {...props}/>}
           </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
